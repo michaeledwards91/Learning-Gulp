4c66027301d1cd1ccbf6f5a81b5b7413083ef645
michaeledwards91 <michael.edwards1991@gmail.com>
2017-03-07 03:10:39 -0600

    Added compass, browser-sync, and html tasks

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,57 @@
 // Require modules
 var gulp = require("gulp");
 var uglify = require("gulp-uglify");
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
 var rename = require("gulp-rename");
+var plumber = require("gulp-plumber"); //.pipe(plumber()) must go right after gulp.src
+var autoprefixer = require("gulp-autoprefixer");
+var compass = require("gulp-compass");
 
 // Scripts
 gulp.task('scripts', function() {
 	gulp.src(['app/js/**/*.js', '!app/js/**/*min.js'])
-	.pipe(rename({suffix:'.min'}))
-	.pipe(uglify())
-	.pipe(gulp.dest('app/js'));
+		.pipe(plumber())
+		.pipe(rename({suffix:'.min'}))
+		.pipe(uglify())
+		.pipe(gulp.dest('app/js'));
+});
+
+// Compass / Sass Tasks
+gulp.task('compass', function() {
+	gulp.src('app/scss/style.scss')
+		.pipe(plumber())
+		.pipe(compass({
+			config_file: '/config.rb',
+			css: 'app/css',
+			sass: 'app/scss',
+			require: ['susy']
+		}))
+		.pipe(autoprefixer('last 2 versions'))
+		.pipe(gulp.dest('app/css/'))
+		.pipe(reload({stream:true}));
+});
+
+// HTML Tasks
+gulp.task('html', function() {
+	gulp.src('app/**/*.html');
+});
+
+// Browser-Sync Tasks
+gulp.task('browser-sync', function() {
+	browserSync({
+		server:{
+			baseDir: "./app/"
+		}
+	});
+});
+
+// Watch Tasks
+gulp.task('watch', function() {
+	gulp.watch('app/js/**/*.js', ['scripts']);
+	gulp.watch('app/scss/**/*.scss', ['compass']);
+	gulp.watch('app/**/*.html', ['html']);
 });
 
 // Default Task
-gulp.task('default', ['scripts']);
+gulp.task('default', ['scripts', 'compass', 'html', 'browser-sync', 'watch']);
